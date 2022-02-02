7528ee4657aacb38dee73d2e1610835ddd0bf73b
Chris Schiebelbein <c_schiebelbein@hotmail.com>
2022-02-01 21:34:37 -0700

    still attempting to fix chatbox

--- src/App.js
+++ src/App.js
@@ -38,12 +38,12 @@ function App() {
 </div>
 <div className="chatdisplay">
 <iframe id="twitch-chat-embed"
-        src="https://www.twitch.tv/embed/eviljimx/chat?parent=anypercentmedia.com"
+        src="https://www.twitch.tv/embed/eviljimx/chat?parent=https://www.anypercentmedia.com/"
         height="400"
         width="50%">
 </iframe>
 <iframe id="twitch-chat-embed"
-        src="https://www.twitch.tv/embed/theamericandweem/chat?parent=anypercentmedia.com"
+        src="https://www.twitch.tv/embed/theamericandweem/chat?parent=https://www.anypercentmedia.com/"
         height="400"
         width="50%">
 </iframe>
